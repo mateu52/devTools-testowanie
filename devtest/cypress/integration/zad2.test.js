ae295c1065451c3d341d6dfa352b1dd868799594
mateu52 <wuoelte@gmail.com>
2022-05-12 08:10:31 +0200

    testy cy

--- devtest/cypress/integration/zad2.test.js
+++ devtest/cypress/integration/zad2.test.js
@@ -1,5 +1,16 @@
 describe('sum test E2E', () => {
     it('clicks inputs' , () => {
-        cy.visit('http://localhost:3000/')
+        cy.visit('http://localhost:3000');
+        cy.get('input[type="number"]')
+            .type('22')
+        cy.get('button[type="submit"]').click()
+        cy.get('button').contains('Reset').click()
+        cy.get('button').contains('+').click()
+        cy.get('button').contains('+').click()
+        cy.get('button').contains('+').click()
+        cy.get('button').contains('-').click()
+        cy.get('button').contains('-').click()
+            
     })
+    
 })
